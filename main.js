c03b84b947bd5cabf6fff8e0eddf3e1a05152abc
Soham-1111 <73647733+Soham-1111@users.noreply.github.com>
2021-01-22 18:06:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function calculate(){
+    console.log("My First Console Project");
+    var first_name="Soham";
+            var last_name="Mehta";
+            var full_name=first_name.concat(last_name);
+            console.log(full_name);
+first=20;
+second=100;
+             
+            var add_number=first+second;
+            console.log(add_number);
+
+            var sub_number=second-first;
+            console.log(sub_number);
+
+            var mul_number=first*second;
+            console.log(mul_number);
+
+            var divide_number= second/first;
+            console.log(divide_number);
+
+            
+
+           
+}
+
+
